eb64c747588d86748f0e5148a82dceb2b8c6b80a
Alfredo <21030761@itcelaya.edu.mx>
2024-05-13 19:28:48 -0600

    UI fixed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-
+      <br/>
       <Testimony
         image={'jesse-pinkman'}
         name={'Jesse Pinkman'}
@@ -14,7 +14,7 @@ function App() {
         charge={'Ing. de Software'}
         company={'Los Pollos Hermanos'}
         testimony={'Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.'}
-      />
+      /><br/>
       <Testimony
         image={'skyler'}
         name={'Skyler White'}
@@ -22,7 +22,7 @@ function App() {
         charge={'Ing. de Software'}
         company={'Amazon'}
         testimony={'freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'}
-      />
+      /><br/>
       <Testimony
         image={'walter-white'}
         name={'Walter White'}
@@ -30,7 +30,7 @@ function App() {
         charge={'Ing. de Software'}
         company={'Los Pollos Hermanos'}
         testimony={'Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'}
-      />
+      /><br/>
     </div>
   );
 }
